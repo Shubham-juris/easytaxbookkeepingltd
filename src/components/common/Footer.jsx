0ff7b16c807cc37f1395f856d00dd95a9f90a726
Shubham Kumar <shubham.developerjuris@gmail.com>
2025-05-06 11:57:24 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -6,9 +6,10 @@ const Footer = () => {
       <footer className="bg-gray-100 text-center py-6">
         <div className="mb-4">
           <h2 className="text-xl font-semibold">EASYTAX AND BOOKKEEPING</h2>
+          <p>>www.easytaxbookkeeping.com</p>
         </div>
         <div>
-          <p className="text-sm text-gray-600">2115 27 Ave NE Calgary AB T2E 7E4 <br></br>www.easytaxbookkeeping.com</p>
+          <p className="text-sm text-gray-600">2115 27 Ave NE Calgary AB T2E 7E4 </p>
         </div>
       </footer>
     </>
